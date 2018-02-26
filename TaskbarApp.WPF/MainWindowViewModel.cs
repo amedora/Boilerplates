7f7a3c5825b701f66d8dee77bfc7532a8f8b2c79
amedora <americandragsterracing@gmail.com>
2018-02-26 13:49:26 +0900

    タスクバーアイコンのコンテキストメニューからアプリケーションの終了ができる。

--- TaskbarApp.WPF/MainWindowViewModel.cs
+++ TaskbarApp.WPF/MainWindowViewModel.cs
@@ -3,10 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using ReactiveUI;
 
 namespace TaskbarApp.WPF
 {
     class MainWindowViewModel
     {
+        #region コマンド
+        private ReactiveCommand _ApplicationExitCommand;
+        public ReactiveCommand ApplicationExitCommand => _ApplicationExitCommand;
+        #endregion
+
+        public MainWindowViewModel()
+        {
+            InitializeCommand();
+        }
+
+        private void InitializeCommand()
+        {
+            _ApplicationExitCommand = ReactiveCommand.Create(() => Application.Current.Shutdown());
+        }
     }
 }
